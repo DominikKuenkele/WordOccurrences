22ca1ac8fb0f26b63e36f91bf4b65157aa39f432
Dominik Künkele <dominik-kuenkele@outlook.com>
2022-06-27 16:48:42 +0200

    translations, restructuring, exporting by POS

--- translate.py
+++ translate.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from collections import defaultdict
 
 import dill
@@ -38,14 +39,23 @@ def translate(source_folder, target_file):
         occurrences_batch.append(occurrences)
 
         for index, source_text in enumerate(source_batch):
-            translated_batch.append(translator.translate(source_text, src=SOURCE_LANG, dest=TARGET_LANG))
             print(pos, index)
+            delays = [0, 5, 10, 20, 30]
+            for delay in delays:
+                try:
+                    if delay > 0:
+                        print(f'Waiting {delay}s...')
+                    time.sleep(delay)
+                    translated_batch.append(translator.translate(source_text, src=SOURCE_LANG, dest=TARGET_LANG))
+                    break
+                except Exception:
+                    continue
 
         for batch_index, translated_text in enumerate(translated_batch):
             separated_source_words = source_batch[batch_index].split('\n')
             separated_translated_words = translated_text.text.split('\n')
             for word_index, translated_word in enumerate(separated_translated_words):
-                translated_dictionary[pos][(separated_source_words[word_index], translated_word)] = \
+                translated_dictionary[pos][(separated_source_words[word_index], translated_word.lower())] = \
                     occurrences_batch[batch_index][word_index]
 
     with open(target_file, 'wb') as f:
